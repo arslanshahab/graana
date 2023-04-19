2e837d030d2195ef0ae71c4c39d0537a8fb45942
Arslan Shahab <arslanshahab@gmail.com>
2023-04-19 13:10:45 +0500

    added navbar and hero setion UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components/Button";
-import { Title } from "./components/Title";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
 
 function App() {
   return (
     <React.Fragment>
-      <h1>FOOTER</h1>
-      <Button />
-      <Title />
+      <Navbar />
+      <Hero />
     </React.Fragment>
   );
 }
