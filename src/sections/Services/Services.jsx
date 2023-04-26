1f9a7d23d4dd6d0258d18bb336e6a1ce86379444
Arslan Shahab <arslanshahab@gmail.com>
2023-04-26 10:38:58 +0500

    fixed blur effect for services section

--- src/sections/Services/Services.jsx
+++ src/sections/Services/Services.jsx
@@ -5,31 +5,34 @@ import invest from "./../../assets/images/invest.webp";
 
 export const Services = () => {
   return (
-    <div className={styles.services_wrapper}>
-      <DescriptionCard
-        image={invest}
-        title="Invest"
-        description="Invest in fully legal Imarat projects. Our “Ownerships & Approvals, Demand & Delivery” approach ensures fantastic returns with full risk mitigation"
-        btnTitle="Invest"
-        btnVariant="outline"
-        btnType="primary"
-      />
-      <DescriptionCard
-        image={invest}
-        title="Invest"
-        description="Invest in fully legal Imarat projects. Our “Ownerships & Approvals, Demand & Delivery” approach ensures fantastic returns with full risk mitigation"
-        btnTitle="Invest"
-        btnVariant="outline"
-        btnType="primary"
-      />
-      <DescriptionCard
-        image={invest}
-        title="Invest"
-        description="Invest in fully legal Imarat projects. Our “Ownerships & Approvals, Demand & Delivery” approach ensures fantastic returns with full risk mitigation"
-        btnTitle="Invest"
-        btnVariant="outline"
-        btnType="primary"
-      />
+    <div className={styles.services_section}>
+      <div className={styles.box}></div>
+      <div className={styles.services_wrapper}>
+        <DescriptionCard
+          image={invest}
+          title="Invest"
+          description="Invest in fully legal Imarat projects. Our “Ownerships & Approvals, Demand & Delivery” approach ensures fantastic returns with full risk mitigation"
+          btnTitle="Invest"
+          btnVariant="outline"
+          btnType="primary"
+        />
+        <DescriptionCard
+          image={invest}
+          title="Invest"
+          description="Invest in fully legal Imarat projects. Our “Ownerships & Approvals, Demand & Delivery” approach ensures fantastic returns with full risk mitigation"
+          btnTitle="Invest"
+          btnVariant="outline"
+          btnType="primary"
+        />
+        <DescriptionCard
+          image={invest}
+          title="Invest"
+          description="Invest in fully legal Imarat projects. Our “Ownerships & Approvals, Demand & Delivery” approach ensures fantastic returns with full risk mitigation"
+          btnTitle="Invest"
+          btnVariant="outline"
+          btnType="primary"
+        />
+      </div>
     </div>
   );
 };
