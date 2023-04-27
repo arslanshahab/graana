2ccbd97fb74810653c5fbe6eb9e3f1b067e2bb21
Kamran Ali <km775.ali@gmail.com>
2023-04-27 20:35:27 +0500

    blog section using props

--- src/components/Blogs/BlogItem.jsx
+++ src/components/Blogs/BlogItem.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import img from "../../assets/images/Hospital-Emergency-Block.webp";
-import icon from "../../assets/images/blogUserDummyImage.svg"
 import styles from "./BlogItem.module.scss";
-const BlogItem = () => {
+const BlogItem = ({ img, bhead, btitle, btext, bdimg, bwriter, bdate }) => {
   return (
     <div className={styles.blog_item}>
       <div >
-        <img src={img} alt="Hospital Emergency block" className={styles.img}/>
+        <img src={img} alt={btitle} className={styles.img} />
       </div>
       <div className={styles.blog_body}>
-      <div className={styles.blog_body_sec}>
-        <div className={styles.blog_text}>
-          <h6>Article</h6>
-          <h2>Ganga Ram Hospital Lahore:</h2>
-          <p>Ganga Ram Hospital is a premier healthcare institute that was established in 1921 by Sir Ganga Ram, a Phlianthropist and...</p>
-        </div>
-        <div className={styles.blog_writer}>
-           <div className={styles.blog_writer_img}>
-             <img src={icon} alt="dummy icon" />
-           </div>
-           <div className={styles.blog_writer_detail}>
-            <h5>Mariana Aslam</h5>
-            <span>April 19,2023 7min read</span>
-           </div>
-           </div>
+        <div className={styles.blog_body_sec}>
+          <div className={styles.blog_text}>
+            <h6>{bhead}</h6>
+            <h2>{btitle}</h2>
+            <p>{btext}</p>
+          </div>
+          <div className={styles.blog_writer}>
+            <div className={styles.blog_writer_img}>
+              <img src={bdimg} alt={bwriter} />
+            </div>
+            <div className={styles.blog_writer_detail}>
+              <h5>{bwriter}</h5>
+              <span>{bdate}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Blogs/BlogList.jsx
+++ src/components/Blogs/BlogList.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import BlogItem from './BlogItem';
+import { Button } from "../Button/Button"
 import styles from "./BlogList.module.scss";
+import bimg1 from "../../assets/images/Hospital-Emergency-Block.webp";
+import bimg2 from "../../assets/images/blogimage2.webp";
+import bimg3 from "../../assets/images/blogimage3.webp"
+import iconImg from "../../assets/images/blogUserDummyImage.svg"
 
 const BlogList = () => {
   return (
@@ -8,10 +13,37 @@ const BlogList = () => {
       <h2>Graana Blogs</h2>
       <p>Trends and perspectives for buyers and investors</p>
       <div className={styles.blog_list}>
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        <BlogItem
+          img={bimg1}
+          bhead="Article"
+          btitle="Ganga Ram Hospital Lahore:"
+          btext="Ganga Ram Hospital is a premier healthcare institute that was established in 1921 by Sir Ganga Ram, a Phlianthropist and..."
+          bdimg={iconImg}
+          bwriter="Mariana Aslam"
+          bdate="April 19,2023 7min read"
+        />
+        <BlogItem
+          img={bimg2}
+          bhead="Article"
+          btitle="Karachi Cantt Station: History,..."
+          btext="Karachi Cantt Station is one of the most important and busiest railway station in Pakistan, located near Dr. Daudpota Road,..."
+          bdimg={iconImg}
+          bwriter="Mariana Aslam"
+          bdate="April 19,2023 8min read"
+        />
+        <BlogItem
+          img={bimg3}
+          bhead="Article"
+          btitle="Dubai Introduces Service to Verify..."
+          btext="ISLAMABAD: Dubai Land Department (DLD) has announced the launch of a new electronic service called Madmoun,which aims to..."
+          bdimg={iconImg}
+          bwriter="Laila Waqar"
+          bdate="April 19,2023 1min read"
+        />
       </div>
+
+      <Button type="secondary" size="lg" title="VIEW ALL BLOGS" />
+
     </div>
   )
 }
